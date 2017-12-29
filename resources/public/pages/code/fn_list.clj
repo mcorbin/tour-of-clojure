e0021bd62c64a31442da84441aef713c8b48c1f8
mcorbin <corbin.math@gmail.com>
2017-12-29 17:42:55 +0100

    List page

--- resources/public/pages/code/fn_list.clj
+++ resources/public/pages/code/fn_list.clj
@@ -1,1 +1,11 @@
-(+ 1 2)
+;; add a value
+(println (conj '(1 2 3) 4))
+
+;; return the first value
+(println (peek '(1 2 3)))
+
+;; return a new list without the first value
+(println (pop '(1 2 3)))
+
+;; get the value at index n
+(println (nth '(1 2 3) 2))
--- src/tourofclojure/pages/fn_list.clj
+++ src/tourofclojure/pages/fn_list.clj
@@ -9,8 +9,24 @@
   [page-number lang]
   (condp = lang
     "fr" [:div
-          [:h2 "Opérations sur les listes"]
-          [:p "Voici quelques opérations sur les listes."]
+          [:h2 "Opérations sur les list"]
+          [:p "Voici quelques opérations sur les list."]
+          [:p "Vous allez maintenant sûrement vous demander: à quoi servent les"
+           " list alors que l'on a déjà les vector ?"]
+          [:p "Tout d'abord, vous pouvez voir que contrairement à un vector,"
+           " les list \"grandissent\" par la gauche. Les nouvelles valeurs sont"
+           " ajoutées au début, contrairement au vector où elles sont ajoutées à"
+           " la fin."]
+          [:p "De la même manière, les fonctions comme " [:b "peek"] " et "
+           [:b "pop"] " retournent ici le premier élément de la liste."]
+          [:p "Enfin, les list ont des performances différentes des vector."
+           " Par exemple, " [:b "nth"] " est très lent par rapport au " [:b "get"]
+           " des vector (vous pouvez voir les listes comme des listes chainées là"
+           " où les vector peuvent accéder à une valeur directement)."]
+          [:p "En général, vous utiliserez rarement les list, sauf quand vous"
+           " voulez qu'une structure de données où les valeurs sont ajoutées ou"
+           " supprimées au début. Les list sont également utilisées pour écrire des "
+           [:b "macro"] ", un sujet que nous découvrirons plus tard."]
           (navigation-block page-number)]
     [:h2 "Language not supported."]))
 
