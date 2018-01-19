eb810a0f8bc17cbb1417ed1aaac80e0b41c6607b
mcorbin <corbin.math@gmail.com>
2018-01-19 19:20:43 +0100

    condp page and code

--- resources/public/pages/code/condp.clj
+++ resources/public/pages/code/condp.clj
@@ -1,1 +1,15 @@
-(+ 1 2)
+;; with a matching close
+(println (condp = 1
+           2 "1 == 2 ?!"
+           1 "1 == 1 !"))
+
+;; Another example with an anonymous function.
+(println (condp #(= (:foo %1) %2) 10
+           {:foo 1} ":foo == 1 ?!"
+           {:foo 10} ":foo == 10 !"))
+
+;; without matching clause, default valueo
+(println (condp = 1
+           2 "1 == 2 ?!"
+           3 "3 == 1 !"
+           "default value"))
--- src/tourofclojure/pages/condp.clj
+++ src/tourofclojure/pages/condp.clj
@@ -10,7 +10,21 @@
   (condp = lang
     "fr" [:div
           [:h2 "Condp"]
-          (navigation-block page-number)]
+          [:p "Une autre construction intéressante est " [:b "condp"] "."]
+          [:p "Le premier paramètre de " [:b "condp"] " est une fonction que l'on appellera " [:b "f"] "."
+           " Le second paramètre un valeur que l'on appellera " [:b "v"] ". "
+           [:b "condp"] " prend ensuite une série de clauses où une valeur "
+           [:b "c"] " est associée à un résultat " [:b "r"] "."
+           ]
+          [:p "Pour chaque clause de façon séquentielle, " [:b "condp"] " appellera"
+           [:b "(f c v)"] " et retournera " [:b "r"] " si le résultat est vrai."]
+          [:p "Ici aussi, il est possible de donner une valeur par défaut si aucune"
+           " clause ne match. Cela se fait en indiquant simplement en dernier paramètre"
+           " la valeur par défaut."]
+          [:p [:b "Attention"] ", contrairement à " [:b "cond"] " vu précédemment, "
+           [:b "condp"] " retournera une exception si aucune clause ne match et "
+           "qu'aucune valeur par défaut n'est fournie."]
+          ]
 
     [:h2 "Language not supported."]))
 
