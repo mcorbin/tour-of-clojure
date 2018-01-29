1ded016989602ebbec4185d5eef09afb22352437
mcorbin <corbin.math@gmail.com>
2018-01-29 22:33:32 +0100

    start error handling page

--- resources/public/pages/code/error.clj
+++ resources/public/pages/code/error.clj
@@ -1,1 +1,16 @@
-(+ 2 3)
+(try
+  (throw (js/Error. "My exception"))
+  (catch js/Error err
+    (println "Caught " err))
+  (finally
+    (println "Finally block" "\n")))
+
+(try
+  (throw "My exception is a string")
+  (catch js/Object err
+    (println "Caught a string: " err "\n")))
+
+(try
+  (throw "My exception is a string")
+  (catch :default err
+    (println "Caught everything: " err)))
--- src/tourofclojure/pages/error.clj
+++ src/tourofclojure/pages/error.clj
@@ -9,7 +9,13 @@
   (condp = lang
     "fr" [:div
           [:h2 "Gestion des erreurs"]
-          [:p "Exception"]
+          [:p "De la même façon que sa plateforme hôte, la gestion des erreurs"
+           " en Clojure se base sur des exceptions."]
+          [:p "Une exception peut être capturée via un block " [:b "try-catch"]
+           " terminé eventuellement d'un block " [:b "finally"] "."]
+          [:p "Une opération illégale où le mot clé " [:b "throw"] " lèvera"
+           " une exception."
+           ]
           (navigation-block page-number)]
 
     [:h2 "Language not supported."]))
