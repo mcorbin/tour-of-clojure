67258f5f4d02d695f368897596818e5520fd306a
mcorbin <corbin.math@gmail.com>
2018-02-23 19:24:52 +0100

    apply

--- resources/public/pages/code/apply.clj
+++ resources/public/pages/code/apply.clj
@@ -1,4 +1,3 @@
-
 (println (apply + [1 2 3 4 5]) "\n")
 
-(println (apply  [1 2 3 4 5]) "\n")
+(println (apply (partial conj [1 2 3]) [4 5 6 7]) "\n")
--- src/tourofclojure/pages/apply.clj
+++ src/tourofclojure/pages/apply.clj
@@ -10,7 +10,18 @@
   (condp = lang
     "fr" [:div
           [:h2 "apply"]
-          [:p [:b "apply"]]
+          [:p [:b "apply"] " permet de passer à une fonction ses paramètres"
+           " sous forme de liste. Par exemple:"]
+          [:pre [:code "(apply + [1 2 3 4 5])"]]
+          [:p " est équivalent à :"]
+          [:pre [:code "(+ 1 2 3 4 5)"]]
+          [:p "Les paramètres, ici sous forme de vector, sont passées à la fonction "
+           [:b "+"] "."]
+          [:p "Là aussi, l'utilisation de " [:b "partial"] " peut avoir son utilité:"]
+          [:pre [:code "(apply (partial conj [1 2 3]) [4 5 6 7])"]]
+          [:p "Ce code utilise " [:b "partial"] " pour créer une fonction"
+           " qui ajoutera au vector " [:b "[1 2 3]"] " les paramètres passées."
+           "On peut ensuite appeler cette fonction avec " [:b "apply"] "."]
           (navigation-block page-number)]
     [:h2 "Language not supported."]))
 
