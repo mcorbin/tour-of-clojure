615014b9482f6b1fb1b8c970a7d180fe9471b0a4
mcorbin <corbin.math@gmail.com>
2018-07-01 18:46:33 +0200

    More details for cond

--- src/tourofclojure/pages/cond.clj
+++ src/tourofclojure/pages/cond.clj
@@ -18,7 +18,21 @@
            ", tout vaut vrai. Vous pouvez donc facilement définir une clause par"
            " défaut, toujours évaluée à vrai, via un keyword par exemple."]
           [:p "Si aucune clause n'est vraie, cond retourne "  [:b "nil"]]
-          (navigation-block previous next)]
+          (navigation-block previous next)
+          [:h2 "Un exemple"]
+          [:pre [:code
+                 "(println (cond
+           (= 1 2) \"1 == 2 ?!\"
+           (= 1 3) \"1 == 3 ?!\"
+           :default \"A default value\"))"]]
+          [:p "On voit ici que l'on teste plusieurs conditions. Aucune condition"
+           " n'est valide, ce sera donc la condition par défaut qui sera "
+           "exécutée et cette expression retournera la chaîne "
+           [:b "A default value"] "."]
+          [:p "En effet, comme vu précédemment, un keyword est considéré"
+           " comme " [:b "true"] ". Le résultat serait le même avec ue chaîne,"
+           " un nombre ... excepté " [:b "nil"] " et " [:b " false"] "."]
+          ]
 
     [:h2 "Language not supported."]))
 
