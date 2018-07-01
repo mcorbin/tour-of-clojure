17c23845866d5844f246038378ad7d5f665da89d
mcorbin <corbin.math@gmail.com>
2018-07-01 19:00:43 +0200

    for: more details

--- src/tourofclojure/pages/for.clj
+++ src/tourofclojure/pages/for.clj
@@ -12,9 +12,19 @@
     "fr" [:div
           [:h2 "for"]
           [:p [:b "for"] " est utilisée pour faire de la compréhension de liste."]
-          [:p "Il est semblable à " (link-to {} "/pages/26" "doseq") " sauf "
+          [:p "Il est semblable à " (link-to {} "/pages/doseq" "doseq") " sauf "
            " qu'ici " [:b "for"] " construira une séquence à partir du résultat"
            " de chaque itération."]
+          [:pre [:code "(for [value [0 1 2 3 4]]
+  (* value 10))"]]
+          [:p "Dans cet exemple, on appelle " [:b "for"] " avec un vector"
+           " d'entiers. On multiplie ensuite " [:b "value"] " (qui représente "
+           "l'élément en cours de traitement) par 10. Le résultat sera"
+           " le vector initial avec chaque élément multiplié par 10."]
+          [:p "De manière générale, préférez utiliser "
+           (link-to {} "/pages/map" "map") " à la place de " [:b "for"]
+           " pour ce genre de traitements simples, ou "
+           (link-to {} "/pages/filter" "filter") " à la place de " [:b ":when"] "."]
           (navigation-block previous next)]
     [:h2 "Language not supported."]))
 
