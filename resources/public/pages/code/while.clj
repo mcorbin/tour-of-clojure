22604ac0b52cb25124d8ececf41bdda2cf692e41
mcorbin <corbin.math@gmail.com>
2019-03-30 19:14:40 +0100

    while

--- resources/public/pages/code/while.clj
+++ resources/public/pages/code/while.clj
@@ -1,1 +1,5 @@
-(+ 1 2)
+(def state (atom [1 2 3 4 5]))
+
+(while (not-empty @state)
+  (println (last @state))
+  (swap! state pop))
--- src/tourofclojure/pages/while.clj
+++ src/tourofclojure/pages/while.clj
@@ -12,8 +12,23 @@
     "fr" [:div
           [:h2 "while"]
           [:p [:b "while"] " permet de répéter une action (généralement un effet de "
-           " bord) tant qu'une condition est vraie."]
-          [:p ""]
+           " bord) tant qu'une condition est vraie. Avant de lire cette page,"
+           " prenez un peu d'avance et regardez comment les " (link-to {} "/pages/types" "Atom") " fonctionnent"]
+          [:pre [:code "(def state (atom [1 2 3 4 5]))
+
+(while (not-empty @state)
+  (println (last @state))
+  (swap! state pop))
+"]]
+          [:p "Dans cet exemple, nous définissons un atom contenant un "
+           "vector de 5 éléments. Nous appelons ensuite " [:b "while"] " avec"
+           " une condition d'arrêt (qui est ici le fait que le vector soit"
+           " vide)."]
+          [:p "A chaque itération, " [:b "println"] " est appelé avec comme"
+           " paramètre la dernière valeur du vector, puis celle ci est supprimée"
+           " en appelant " [:b "pop"] " sur l'atom."]
+          [:p "Une fois le vector contenu dans l'atom vide, " [:b "while"]
+           " retourne " [:b "nil"] "."]
           (navigation-block previous next)]
     [:h2 "Language not supported."]))
 
